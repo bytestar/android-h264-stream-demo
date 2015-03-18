74f65de4d6a70783dab182ea38602fed1fbfc8b5
bytestar <lucas.pctseng@gmail.com>
2015-03-18 14:28:13 +0800

    init commit

---
+++ AndroidH264StreamDemo/src/com/jemi/androidh264streamdemo/MainActivity.java
@@ -0,0 +1,392 @@
+package com.jemi.androidh264streamdemo;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.graphics.ImageFormat;
+import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
+import android.hardware.Camera.Size;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends Activity implements SurfaceHolder.Callback, PreviewCallback 
+{
+	private final static String TAG = MainActivity.class.getSimpleName();
+	
+	private final static String SP_CAM_WIDTH = "cam_width";
+	private final static String SP_CAM_HEIGHT = "cam_height";
+	private final static String SP_DEST_IP = "dest_ip";
+	private final static String SP_DEST_PORT = "dest_port";
+	
+	private final static int DEFAULT_FRAME_RATE = 15;
+	private final static int DEFAULT_BIT_RATE = 500000;
+	
+	Camera camera;
+	SurfaceHolder previewHolder;	
+	byte[] previewBuffer;
+	boolean isStreaming = false;
+	AvcEncoder encoder;
+	DatagramSocket udpSocket;
+	InetAddress address;
+	ArrayList<byte[]> encDataList = new ArrayList<byte[]>();
+	ArrayList<Integer> encDataLengthList = new ArrayList<Integer>();
+	
+	Runnable senderRun = new Runnable() {
+		@Override
+		public void run()
+		{
+			while (isStreaming)
+			{
+				boolean empty = false;
+				byte[] encData = null;
+				
+				synchronized(encDataList)
+				{
+					if (encDataList.size() == 0)
+					{
+						empty = true;
+					}
+					else
+						encData = encDataList.remove(0);
+				}
+				if (empty)
+				{
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					continue;
+				}
+		        try 
+		        {         
+		        	DatagramPacket packet = new DatagramPacket(encData, encData.length, address, 5000);  
+		            udpSocket.send(packet);  
+		        }
+		        catch (IOException e)  
+		        {  
+		          	e.printStackTrace();
+		        }				
+			}
+			//TODO:
+		}
+	};
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.setContentView(R.layout.activity_main);
+        
+        this.findViewById(R.id.btnCamSize).setOnClickListener(
+        	new View.OnClickListener() 
+        	{
+				@Override
+				public void onClick(View v) 
+				{
+					showSettingsDlg();
+				}
+			});
+        
+        this.findViewById(R.id.btnStream).setOnClickListener(
+            	new View.OnClickListener() 
+            	{
+    				@Override
+    				public void onClick(View v) 
+    				{
+    					if (isStreaming)
+    					{
+    						((Button)v).setText("Stream");
+    						stopStream();
+    					}
+    					else
+    					{
+    						showStreamDlg();
+    					}
+    				}
+    			});        
+        
+        SurfaceView svCameraPreview = (SurfaceView) this.findViewById(R.id.svCameraPreview);        
+        this.previewHolder = svCameraPreview.getHolder();
+        this.previewHolder.addCallback(this);     
+    }
+    
+    @Override
+    protected void onPause() 
+    {
+    	this.stopStream();
+    	
+
+        if (encoder != null)
+        	encoder.close();
+        
+    	super.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) 
+    {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) 
+    {
+        int id = item.getItemId();
+        if (id == R.id.action_settings)
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+	@Override
+	public void onPreviewFrame(byte[] data, Camera camera) 
+	{
+		this.camera.addCallbackBuffer(this.previewBuffer);
+		
+		if (this.isStreaming)
+		{
+			if (this.encDataLengthList.size() > 100)
+			{
+				Log.e(TAG, "OUT OF BUFFER");
+				return;
+			}
+			
+			byte[] encData = this.encoder.offerEncoder(data);
+			if (encData.length > 0)
+			{
+				synchronized(this.encDataList)
+				{
+					this.encDataList.add(encData);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) 
+	{
+		startCamera();
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) 
+	{
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder)
+	{
+		stopCamera();
+	}
+	
+	private void startStream(String ip, int port)
+	{
+		SharedPreferences sp = this.getPreferences(Context.MODE_PRIVATE);
+        int width = sp.getInt(SP_CAM_WIDTH, 0);
+        int height = sp.getInt(SP_CAM_HEIGHT, 0);        
+        
+        this.encoder = new AvcEncoder();
+        this.encoder.init(width, height, DEFAULT_FRAME_RATE, DEFAULT_BIT_RATE);
+        
+        try
+        {  
+        	this.udpSocket = new DatagramSocket();  
+            this.address = InetAddress.getByName(ip);  
+        }
+        catch (SocketException e)
+        {  
+            // TODO Auto-generated catch block  
+        	e.printStackTrace();
+        	return;
+        }
+        catch (UnknownHostException e) 
+        {  
+            // TODO Auto-generated catch block
+        	e.printStackTrace();
+        	return;
+        }  
+		sp.edit().putString(SP_DEST_IP, ip).commit();
+		sp.edit().putInt(SP_DEST_PORT, port).commit();
+		
+        this.isStreaming = true;
+        Thread thrd = new Thread(senderRun);
+        thrd.start();
+        
+		((Button)this.findViewById(R.id.btnStream)).setText("Stop");		
+		this.findViewById(R.id.btnCamSize).setEnabled(false);
+	}
+	
+	private void stopStream()
+	{
+		this.isStreaming = false;
+		
+		if (this.encoder != null)
+			this.encoder.close();
+		this.encoder = null;
+		
+		this.findViewById(R.id.btnCamSize).setEnabled(true);
+	}
+	
+	private void startCamera()
+	{        
+		SharedPreferences sp = this.getPreferences(Context.MODE_PRIVATE);
+        int width = sp.getInt(SP_CAM_WIDTH, 0);
+        int height = sp.getInt(SP_CAM_HEIGHT, 0);
+        if (width == 0)
+        {
+        	Camera tmpCam = Camera.open();  
+        	Camera.Parameters params = tmpCam.getParameters();  
+    		final List<Size> prevSizes = params.getSupportedPreviewSizes();
+    		int i = prevSizes.size()-1;
+    		width = prevSizes.get(i).width;
+    		height = prevSizes.get(i).height;    		
+    		sp.edit().putInt(SP_CAM_WIDTH, width).commit();
+    		sp.edit().putInt(SP_CAM_HEIGHT, height).commit();
+    		tmpCam.release();
+    		tmpCam = null;
+        }
+        
+        this.previewHolder.setFixedSize(width, height);
+        
+    	int stride = (int) Math.ceil(width/16.0f) * 16;
+    	int cStride = (int) Math.ceil(width/32.0f)  * 16;
+        final int frameSize = stride * height;
+        final int qFrameSize = cStride * height / 2;
+        
+        this.previewBuffer = new byte[frameSize + qFrameSize * 2];
+        
+        try   
+        {  
+        	camera = Camera.open();  
+        	camera.setPreviewDisplay(this.previewHolder);  
+            Camera.Parameters params = camera.getParameters();  
+            params.setPreviewSize(width, height);  
+            params.setPreviewFormat(ImageFormat.YV12);  
+            camera.setParameters(params);   
+            camera.addCallbackBuffer(previewBuffer);
+            camera.setPreviewCallbackWithBuffer(this);  
+            camera.startPreview();                
+        }
+        catch (IOException e)   
+        {  
+            //TODO:
+        }   
+        catch (RuntimeException e)
+        {
+            //TODO:
+        }        		
+	}
+	
+	private void stopCamera()
+	{
+    	if (camera != null)
+    	{
+			camera.setPreviewCallback(null);
+			camera.stopPreview();   
+	        camera.release();  
+	        camera = null;
+    	}
+	}
+	
+	private void showStreamDlg()
+	{        
+		LayoutInflater inflater = this.getLayoutInflater();
+		View content = inflater.inflate(R.layout.stream_dlg_view, null);
+		
+		SharedPreferences sp = this.getPreferences(Context.MODE_PRIVATE);
+        String ip = sp.getString(SP_DEST_IP, "");
+        int port = sp.getInt(SP_DEST_PORT, -1);
+        if (ip.length() > 0)
+        {
+        	EditText etIP = (EditText)content.findViewById(R.id.etIP);
+        	etIP.setText(ip);
+        	EditText etPort = (EditText)content.findViewById(R.id.etPort);
+        	etPort.setText(String.valueOf(port));
+        }
+		
+		AlertDialog.Builder dlgBld = new AlertDialog.Builder(this);
+		dlgBld.setTitle(R.string.app_name);
+		dlgBld.setView(content);
+		dlgBld.setPositiveButton(android.R.string.ok, 
+			new DialogInterface.OnClickListener() 
+			{
+				@Override
+				public void onClick(DialogInterface dialog, int which)
+				{
+					EditText etIP = (EditText) ((AlertDialog)dialog).findViewById(R.id.etIP);
+					EditText etPort = (EditText) ((AlertDialog)dialog).findViewById(R.id.etPort);
+					String ip = etIP.getText().toString();
+					int port = Integer.valueOf(etPort.getText().toString());
+					if (ip.length() > 0 && (port >=0 && port <= 65535))
+					{
+						startStream(ip, port);
+					}
+					else
+					{
+						//TODO:
+					}					
+				}
+			});
+		dlgBld.setNegativeButton(android.R.string.cancel, null);
+		dlgBld.show();
+	}
+	
+	private void showSettingsDlg()
+	{
+		Camera.Parameters params = camera.getParameters();  
+		final List<Size> prevSizes = params.getSupportedPreviewSizes();
+		String[] choiceStrItems = new String[prevSizes.size()];
+		ArrayList<String> choiceItems = new ArrayList<String>();
+		for (Size s : prevSizes)
+		{
+			choiceItems.add(s.width + "x" + s.height);
+		}
+		choiceItems.toArray(choiceStrItems);
+		
+		AlertDialog.Builder dlgBld = new AlertDialog.Builder(this);
+		dlgBld.setTitle(R.string.app_name);
+		dlgBld.setSingleChoiceItems(choiceStrItems, 0, null);			
+		dlgBld.setPositiveButton(android.R.string.ok, 
+			new DialogInterface.OnClickListener() 
+			{
+				@Override
+				public void onClick(DialogInterface dialog, int which)
+				{
+					int pos = ((AlertDialog)dialog).getListView().getCheckedItemPosition();
+					Size s = prevSizes.get(pos);
+					SharedPreferences sp = MainActivity.this.getPreferences(Context.MODE_PRIVATE);					
+					sp.edit().putInt(SP_CAM_WIDTH, s.width).commit();
+					sp.edit().putInt(SP_CAM_HEIGHT, s.height).commit();
+					
+					stopCamera();
+			        startCamera();
+				}
+			});
+		dlgBld.setNegativeButton(android.R.string.cancel, null);
+		dlgBld.show();
+	}
+}
---
+++ README.md
@@ -0,0 +1,43 @@
+# AndroidH264StreamDemo
+
+This is an example project to show how to streaming from android camera to 
+VLC or gstreamer.
+
+This demo project use MediaCodec API to encode H.264 data
+and simply wrap with UDP packet then send these packets to VLC or gstreamer.
+ 
+The working flow as below
+camera preview data(YV12) -> YUV420sp -> MediaCodec -> H.264 data -> UDP 
+
+## Media Server
+
+By using VLC and gstreamer, we can ensure that the H.264 video streaming is working fine.
+You can use the following command to start a media server.
+
+### VLC
+```
+vlc -vvv udp://@:<port>
+```
+
+### gstreamer
+```
+gst-launch udpsrc port=<port> ! video/x-h264,width=<w>,height=<h>,framerate=<framerate>/1 ! ffdec_h264 ! autovideosink
+```
+ 
+## Requirement
+- Minimal API requirement: 16
+
+## License
+```
+Copyright (C) 2015 Ping-Chun Tseng <lucas.pctseng@gmail.com> 
+Licensed under LGPLv2.1 or later
+```
+
+## Notes
+- In some network environment, UDP packet may be cut by MTU limitation
+
+
+## Future works
+- Support H.264 over RTP (RFC3984)
+- Support NV21 and YUV420p image format
+
